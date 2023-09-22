27867af3046580dcd0a98cd1240ec449f736ad82
aditya95-pixel <136506411+aditya95-pixel@users.noreply.github.com>
2023-09-22 13:44:07 +0530

    Updating the face detection model

--- ml-models/main1.py
+++ ml-models/main1.py
@@ -7,7 +7,9 @@ if not cap.isOpened():
     cap=cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open webcam.")
-while True:
+i=0
+d={}
+while i<=30:
     ret,frame=cap.read()
     result=Deepface.analyze(frame,actions=['emotion'])
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -15,12 +17,17 @@ while True:
     for x,y,w,h in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0),2)
     font=cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame,result['dominant_emotion'],(10,10),font,1,(0,255,255),2)
+    if (result["dominant_emotion"] not in d):
+        d[result["dominant_emotion"]]=1
+    else:
+        d[result["dominant_emotion"]]+=1
+    #cv2.putText(frame,result['dominant_emotion'],(10,10),font,1,(0,255,255),2)
     cv2.imshow("Emotion Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     pickle.dump(result,open("facemodel.pkl","wb"))
     model=pickle.load(open("facemodel.pkl","rb"))
-    
+    i+=1
+print("emotion is",max(d))    
 cap.release()
 cv2.destroyAllWindows()
